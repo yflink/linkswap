7f781fa508ec711905000b116378b390df6ae1d0
Ryan Choi <choi.link227@gmail.com>
2021-01-15 12:06:32 -0500

    add ropsten weth address

--- deploy.js
+++ deploy.js
@@ -64,6 +64,7 @@ if (process.env.NETWORK == 'mainnet') {
   wethAddress = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
 } else if (process.env.NETWORK == 'ropsten') {
   provider = ethers.getDefaultProvider('ropsten');
+  wethAddress = '0xc778417E063141139Fce010982780140Aa0cD5Ab';
 }
 
 let wallet, connectedWallet;
